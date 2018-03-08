a199870b3dda81aca983aae6802f0955150a282c
Akash Pargat <Akash.Pargat@cerner.com>
2018-03-08 07:36:02 -0600

    Updated the method name

--- corvette-drive/src/com/java/datastructure/PalindromePErmutation.java
+++ corvette-drive/src/com/java/datastructure/PalindromePErmutation.java
@@ -1,27 +1,33 @@
 package com.java.datastructure;
 
-public class PalindromePErmutation {
-	public static String palindrome(String str) {
-		char []strArray = str.toCharArray();
-		int index =str.length()-1;
-		for(int i =0; i<str.length(); i++) {
-			if(strArray[i]==' ') {
-				i++;
-			}
-			if(strArray[index]==' ') {
-				index--;
-			}
-			if(strArray[i]!=strArray[index]) {
-				return "The give string is not a palindrome.";
-			}
-			
-			index--;
-		}
-		return "The given string is a palindrome.";
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(palindrome("?eva, can i stab bats in a c,ave?"));
-	}
+public class PalindromePErmutation
+{
+    public static String isPalindrome(String str)
+    {
+        char[] strArray = str.toCharArray();
+        int index = str.length() - 1;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (strArray[i] == ' ')
+            {
+                i++;
+            }
+            if (strArray[index] == ' ')
+            {
+                index--;
+            }
+            if (strArray[i] != strArray[index])
+            {
+                return "The give string is not a palindrome.";
+            }
+
+            index--;
+        }
+        return "The given string is a palindrome.";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(isPalindrome("?eva, can i stab bats in a c,ave?"));
+    }
 }
