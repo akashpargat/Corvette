8bbf5200408dcdcf7a0403562ead514f8be5d715
Akash Pargat <Akash.Pargat@cerner.com>
2018-03-09 07:28:06 -0600

    Updated the code after review.

--- corvette-drive/src/com/java/datastructure/StringCompression.java
+++ corvette-drive/src/com/java/datastructure/StringCompression.java
@@ -1,32 +1,34 @@
 package com.java.datastructure;
 
 /**
- * Input: aaabbbbbbccccccdddaaaaaccccbbbb Output: a3b6c6d3a5c4b4
+ * Input: aaasddddddddcccccccssdddsssppppppssss Output: a3s1d8c7s2d3s3p6s4
  * 
  * @author AP027887
  */
 public class StringCompression
 {
 
-    private String compare(final String stringToCompare)
+    private static void compare(final String stringToCompare)
     {
         int count = 0;
         StringBuffer newString = new StringBuffer();
         for (int i = 0; i < stringToCompare.length(); i++)
         {
-            if (stringToCompare.charAt(i) != stringToCompare.charAt(i + 1))
+            count++;
+            if (i + 1 == stringToCompare.length()
+                    || stringToCompare.charAt(i) != stringToCompare.charAt(i + 1))
             {
                 newString.append(stringToCompare.charAt(i));
                 newString.append(count);
                 count = 0;
             }
-            count++;
         }
-        return stringToCompare;
+        System.out.println(newString);
     }
 
+    // a3s1d8c7s2d3s3p6s4
     public static void main(String[] args)
     {
-
+        compare("aaasddddddddcccccccssdddsssppppppssss");
     }
 }
