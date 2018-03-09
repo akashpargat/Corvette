6111bddad4e2ac883dac75af4d39f5dd4e11a8a1
Akash Pargat <Akash.Pargat@cerner.com>
2018-03-09 11:23:54 -0600

    Added Zero Matrix

--- corvette-drive/src/com/java/datastructure/RotateMatrix.java
+++ corvette-drive/src/com/java/datastructure/RotateMatrix.java
@@ -1,10 +1,19 @@
 package com.java.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class to rotate the Matrix by 90 degree.
  */
+@SuppressWarnings("nls")
 public class RotateMatrix
 {
+    /**
+     * Main Method for entry point.
+     * 
+     * @param args
+     */
     public static void main(String[] args)
     {
         int N = 4;
@@ -19,9 +28,17 @@ public class RotateMatrix
          * int matrix[][] = { {1, 2}, {4, 5} };
          */
 
+        // Test Case 1 for zero Matrix
+        int zeroMatrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 0, 8 }, { 9, 10, 11, 12 },
+                { 13, 0, 15, 16 } };
+
         rotateMatrix(N, matrix);
         // Print rotated matrix
         displayMatrix(N, matrix);
+
+        zeroMatrix(N, zeroMatrix);
+        // Print rotated matrix
+        displayMatrix(N, zeroMatrix);
     }
 
     /**
@@ -67,5 +84,46 @@ public class RotateMatrix
             System.out.print("\n");
         }
         System.out.print("\n");
+    }
+
+    /**
+     * Function to rotate the matrix. (Not an optimized way to do this.)
+     * 
+     * @param n
+     *            Levels in a matrix.
+     * @param matrix
+     *            matrix to print
+     */
+    private static void zeroMatrix(final int n, final int[][] matrix)
+    {
+        List<Integer> row = new ArrayList<>();
+        List<Integer> column = new ArrayList<>();
+        for (int x = 0; x < n; x++)
+        {
+            for (int y = 0; y < n; y++)
+            {
+                if (matrix[x][y] == 0)
+                {
+                    System.out.println(matrix[x][y] + " : (" + x + y + ")");
+                    row.add(x);
+                    column.add(y);
+                }
+            }
+        }
+
+        for (int i = 0; i < row.size(); i++)
+        {
+            for (int y = 0; y < n; y++)
+            {
+                matrix[row.get(i)][y] = 0;
+            }
+        }
+        for (int i = 0; i < column.size(); i++)
+        {
+            for (int y = 0; y < n; y++)
+            {
+                matrix[y][column.get(i)] = 0;
+            }
+        }
     }
 }
